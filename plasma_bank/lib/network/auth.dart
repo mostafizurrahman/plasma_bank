bc29ab336813484155479b77ecdcb496b13cc2de
Mostafizur Rahman <mostafizur.rahman@surecash.net>
2020-06-19 02:17:21 +0600

    stop tracking

--- plasma_bank/lib/network/auth.dart
+++ plasma_bank/lib/network/auth.dart
@@ -1,6 +1,7 @@
 
 
 class Auth{
-  static const String ClientID = "";
-  static const String ClientSecret = "";
+
+  static const String ClientID = "Client-ID 0f3eb0198bdd1b2";
+  static const String ClientSecret = "752506e2c33bcd0bc9703eefa2b097443d4baf3e";
 }
