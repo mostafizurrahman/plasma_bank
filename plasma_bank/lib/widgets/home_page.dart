358a0b72dcb147cfbc307ce801db500a3963e454
Mostafizur Rahman <mostafizur.rahman@surecash.net>
2020-06-29 00:58:09 +0600

    donation home page done

--- plasma_bank/lib/widgets/home_page.dart
+++ plasma_bank/lib/widgets/home_page.dart
@@ -29,8 +29,19 @@ class HomePageWidget extends StatefulWidget {
 class _HomePageState extends State<HomePageWidget> {
   final _db = FirebaseRepositories();
   final _downloader = CovidDataHelper();
+//  AnimationController controller;
+//  Animation<double> animation;
   final _bottomNavigationBehavior = BehaviorSubject<int>();
 //  final PublishSubject csvBehavior = PublishSubject<double>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+//    controller = AnimationController(
+//        duration: const Duration(milliseconds: 1000), vsync: this);
+//    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+  }
 
   @override
   void dispose() {
@@ -46,43 +57,48 @@ class _HomePageState extends State<HomePageWidget> {
 //    final _width = MediaQuery.of(context).size.width;
 //    final _profileWidth = _width * 0.2;
 //    final _profileHeight = _profileWidth * 4 / 3.0;
+    var mediaQuery = MediaQuery.of(context);
+    double _top = mediaQuery.padding.top;
+    double _bottom = mediaQuery.padding.bottom;
     return Container(
       color: Colors.white,
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          body: Container(
-            child: SingleChildScrollView(
-              child: StreamBuilder(
-                stream: this._bottomNavigationBehavior.stream,
-                initialData: 2,
-                builder: (_context, _snap) {
-                  if (_snap.data == 2) {
-                    return _getHomeScreen(_context);
-                  }
-                  return Container(
-                    color: Colors.blue,
-                  );
-                },
-              ),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          child: SingleChildScrollView(
+            child: StreamBuilder(
+              stream: this._bottomNavigationBehavior.stream,
+              initialData: 2,
+              builder: (_context, _snap) {
+                if (_snap.data == 2) {
+                  return _getHomeScreen(_context);
+                }
+                if (_snap.data == 0) {
+                  return _getDonateScreen(_context);
+                }
+                return Container(
+                  color: Colors.blue,
+                );
+              },
             ),
           ),
-          bottomNavigationBar: StreamBuilder(
-            stream: this._bottomNavigationBehavior.stream,
-            initialData: 2,
-            builder: (_context, _snap) {
-              return Container(
-                height: 65,
+        ),
+        bottomNavigationBar: StreamBuilder(
+          stream: this._bottomNavigationBehavior.stream,
+          initialData: 2,
+          builder: (_context, _snap) {
+            return Container(
+              height: (_bottom > 0 ? 55 : 65) + _bottom,
 
 //            color: Colors.grey.withAlpha(15),
-                decoration: AppStyle.bottomNavigatorBox(),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: _getNavigationItems(_context, _snap.data),
-                ),
-              );
-            },
-          ),
+              decoration: AppStyle.bottomNavigatorBox(),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: _getNavigationItems(_context, _snap.data),
+              ),
+            );
+          },
         ),
       ),
     );
@@ -107,7 +123,6 @@ class _HomePageState extends State<HomePageWidget> {
       final _iconName =
           _selectedIndex == i ? _icons[i].replaceAll('_n', '_h') : _icons[i];
       final _widget = Container(
-
         width: _width - 5,
         height: _width - 5,
         child: ClipRRect(
@@ -116,7 +131,10 @@ class _HomePageState extends State<HomePageWidget> {
             color: Colors.white,
             child: Ink(
               child: InkWell(
-                onTap: () => this._bottomNavigationBehavior.sink.add(i),
+                onTap: () {
+                  visible = false;
+                  this._bottomNavigationBehavior.sink.add(i);
+                },
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -159,7 +177,10 @@ class _HomePageState extends State<HomePageWidget> {
   _onTapDonor(final bool _isBloodDonor) {}
 
   _getHomeScreen(BuildContext _context) {
-    final _height = 1300.0;
+    var mediaQuery = MediaQuery.of(context);
+    double _top = mediaQuery.padding.top;
+    double _bottom = mediaQuery.padding.bottom;
+    final _height = 1340.0;
     final _width = MediaQuery.of(_context).size.width;
     final _profileWidth = _width * 0.2;
     final _profileHeight = _profileWidth * 4 / 3.0;
@@ -167,7 +188,7 @@ class _HomePageState extends State<HomePageWidget> {
       width: _width,
       height: _height,
       child: Padding(
-        padding: const EdgeInsets.all(24.0),
+        padding:  EdgeInsets.only(left: 24, right: 24, top:  (_top + 24.0), bottom: 24),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -184,6 +205,87 @@ class _HomePageState extends State<HomePageWidget> {
       ),
     );
   }
+
+  bool visible = false;
+  Widget _getDonateScreen(BuildContext _context) {
+    var mediaQuery = MediaQuery.of(context);
+    double _top = mediaQuery.padding.top;
+    double _bottom = mediaQuery.padding.bottom;
+    final _background = (0.9 * 255).toInt();
+    final _width = MediaQuery.of(context).size.width;
+    if (!this.visible) {
+      Future.delayed(Duration(microseconds: 600), () {
+        this.visible = true;
+        this._bottomNavigationBehavior.sink.add(0);
+      });
+    }
+    return Container(
+      child: AnimatedOpacity(
+        opacity: visible ? 1.0 : 0.0,
+        onEnd: () {
+          debugPrint("this is the end");
+        },
+        duration: Duration(seconds: 1),
+        child: Container(
+          width: _width,
+          height: MediaQuery.of(_context).size.height -
+              50 -
+              _bottom ,
+          //color: Color.fromARGB(255, _background, _background, _background),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, _background, _background, _background),
+            image: DecorationImage(
+              image: ImageHelper.getImageAsset('background_1.jpg'),
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.bottomCenter,
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.only(top: (24 + _top), bottom: 24, left: 24, right: 24),
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              body: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'BECOME A BLOOD DONOR TODAY',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontFamily: AppStyle.fontBold,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 32, bottom: 12),
+                    child: Text(
+
+                      'life saving heroes come in all types and sizes, a single pint can save three lives, a single gesture can create a million smiles',
+                      style: TextStyle(
+                        fontSize: 16,
+
+                        height: 1.3
+                      ),
+
+                    ),
+                  )
+                ],
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: _registerNewDonor,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.add,
+                  color: Color.fromARGB(255, 240, 10, 80),
+                  size: 50,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _registerNewDonor() {}
 
   _onProgress(File _dataFile) {
     this._downloader.readCovidJSON(_dataFile);
